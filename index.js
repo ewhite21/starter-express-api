b3635a9731d475325de1908e7e7814783d29ccbc
ewhite21 <154680924+ewhite21@users.noreply.github.com>
2023-12-23 21:52:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.all('/', (req, res) => {
     console.log("Just got a request!");
-    res.send('Yo!'+process.env.S3_BUCKET);
+    res.send('Yo!'+process.env.TURL);
 })
 app.listen(process.env.PORT || 3000)
