21787fab8f990c678879f7fa1f60fe6415824be1
ewhite21 <154680924+ewhite21@users.noreply.github.com>
2023-12-23 22:20:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ fastify.register(require('@fastify/formbody'));
 fastify.get('/', async function (request, reply) {
     reply.send({ success: true });
 })
+fastify.listen({ port: port }, (err) => {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${port}`);
+})
