659f8f483ad8bcd850efc782e56fc4d2425e378e
ewhite21 <154680924+ewhite21@users.noreply.github.com>
2023-12-23 19:57:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    console.log("Just got a request!");
+    res.send('Yo!'+process.env.S3_BUCKET);
 })
 app.listen(process.env.PORT || 3000)
